d724e2bbedf84a03ed6bd01f9449cbf3c49f41c9
peter-halligan <peter.halligan@mycit.ie>
2020-06-28 18:49:01 +0000

    feat: add script to up load docker image
    upload script takes 2 arguments.
    dockerpath
    version
    
    uploads the image version and tags it as latest

---
+++ upload_docker.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+# to upload an image ensure that DOCKER_USERNAME and DOCKER PASSWORD are set
+# $ export DOCKER_USERNAME=username
+# $ export DOCKER_PASSWORD=password
+# expects 1 arg image name user/repo 
+#  ./upload_docker.sh phalligan/kubernetes-ml-app
+
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath=$1
+version=$2
+
+
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
+docker tag "$dockerpath:$version" "$dockerpath:latest"
+
+# Step 3:
+# Push image to a docker repository
+
+docker push "$dockerpath:$version"
+docker push "$dockerpath:latest"
+
